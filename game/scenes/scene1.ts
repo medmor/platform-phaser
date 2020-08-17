f9b4135c091afc0bc73db53e76c8a1dbbc5294eb
medmor <elmadioune@gmail.com>
2020-08-17 16:52:55 +0100

    2 files modified, 2 added

--- game/scenes/scene1.ts
+++ game/scenes/scene1.ts
@@ -1,31 +1,13 @@
-import Player from "../characters/player"
-import Platforms from "../world/platforms"
-import Thornes from '../world/thornes'
-import Coins from '../world/coins'
-import Inputs from "../utils/inputs"
-import Sound from "../utils/sound"
+import Level from './levelBase'
 
-//import {onCoinTake} from './sharedFunctions'
-
-export default class extends Phaser.Scene {
-
-  inputs: Inputs
-  player: Player
-  platforms: Platforms
-  thornes: Thornes
-  coins: Coins
-  door: Phaser.Geom.Rectangle
-
-  lastTime = 0
+export default class extends Level {
 
   constructor(){
     super('scene1')
   }
 
   init(){
-    this.lastTime = this.time.now
-    this.cameras.main.setBounds(0, 0, 1920, 600);
-    this.physics.world.setBounds(0, 0, 1920, 600, true, true, false, false);
+    super.init();
     (this.scene.get('ui') as any).togglePlayerInventory()
   }
 
@@ -33,62 +15,26 @@ export default class extends Phaser.Scene {
   }
 
   create(){
-    Sound.music.play()
-    this.inputs = new Inputs(this)
+    super.create()
+    this.soundManager.music.play()
 
-    this.platforms = new Platforms(this.physics.world, this)
     this.platforms.scene1()
 
-    this.thornes = new Thornes(this.physics.world, this)
     this.thornes.scene1()
 
-    this.coins = new Coins(this.physics.world, this)
     this.coins.scene1()
     this.coins.addTween(this)
 
     this.door = this.add.image(1800, 424, 'door').getBounds()
 
-
-    this.player = new Player(this, 50, 300, 'player')
-    this.player.initPhysics(this.physics)
-    this.cameras.main.startFollow(this.player);
-
-
-    this.physics.add.collider(this.player, this.platforms) //platforms collision
-    this.physics.add.collider(this.player, this.thornes,()=>{ //thornes collision
-      this.player.thorneDamage()
-    })
-    //this.physics.add.overlap(this.player, this.coins, this.onCoinTake, undefined, this) //coins collision
-
-
   }
 
   update(elapsedTime){
-    if(this.player.visible) this.player.move(this.inputs.horizontale(), this.inputs.verticale(), elapsedTime - this.lastTime)
-
+    super.update(elapsedTime)
     if(this.door.contains(this.player.x, this.player.y)){
       this.scene.start('scene2')
     }
-
-    const coin = this.coins.checkOverllap(this.player)
-    if(coin) this.onCoinTake(coin)
-
-
-    this.lastTime = this.time.now
   }
-
-  onCoinTake(coin){
-    this.player.body.touching.down = false
-    if(coin.active) {
-      coin.active = false
-      this.player.onCoinTake()
-      this.tweens.add({targets: coin, y: "-=200", duration: 500, ease: 'Linear'})
-      .on('complete',()=>{
-        coin.destroy()
-        })
-    }
-  }
-
 
 
 }
--- game/scenes/scene2.ts
+++ game/scenes/scene2.ts
@@ -1,91 +1,39 @@
-import Platforms from "../world/platforms"
-import MovingPlatforms from '../world/movingPlatforms'
-import Thornes from '../world/thornes'
-import Player from "../characters/player"
-import Inputs from "../utils/inputs"
-import Coins from '../world/coins'
+import levelBase from "./levelBase"
 
+export default class extends levelBase {
 
-
-export default class extends Phaser.Scene {
-
-  platforms: Platforms
-  movingPlatforms: MovingPlatforms
-  lastTime = 0
-  player: Player
-  inputs: Inputs
-  coins: Coins
-  thornes: Thornes
-  door: Phaser.Geom.Rectangle
 
   constructor(){
     super('scene2')
   }
 
   init(){
-    this.lastTime = this.time.now
-    this.cameras.main.setBounds(0, 0, 1920, 600);
-    this.physics.world.setBounds(0, 0, 1920, 600, true, true, false, false);
+    super.init()
   }
 
   preload(){
   }
 
   create(){
-    this.platforms = new Platforms(this.physics.world, this)
+    super.create()
     this.platforms.scene2(this)
 
-    this.movingPlatforms = new MovingPlatforms(this.physics.world, this)
     this.movingPlatforms.scene2()
 
-    this.thornes = new Thornes(this.physics.world, this)
     this.thornes.scene2()
 
-    this.coins = new Coins(this.physics.world, this)
     this.coins.scene2()
     this.coins.addTween(this)
 
     this.door = this.add.image(1800, 424, 'door').getBounds()
 
-    this.inputs = new Inputs(this)
-
-    this.player = new Player(this, 50, 300, 'player')
-    this.player.initPhysics(this.physics)
-    this.cameras.main.startFollow(this.player);
-
-    this.physics.add.collider(this.player, this.platforms)
-    this.physics.add.collider(this.player, this.movingPlatforms, this.player.blockThorneDamage, undefined, this.player)
-    this.physics.add.collider(this.player, this.thornes,()=>{ //thornes collision
-      this.player.thorneDamage()
-    })
   }
 
   update(elapsedTime){
-
-    this.movingPlatforms.updateMovingPlatforms()
-
-    if(this.player.visible) this.player.move(this.inputs.horizontale(), this.inputs.verticale(), elapsedTime - this.lastTime)
-
-    let coin = this.coins.checkOverllap(this.player)
-    if(coin) this.onCoinTake(coin)
-
+    super.update(elapsedTime)
     if(this.door.contains(this.player.x, this.player.y)){
       this.scene.start('win')
     }
-
-    this.lastTime = this.time.now
   }
-
-  onCoinTake( coin){
-    if(coin.active) {
-      coin.active = false
-      this.player.onCoinTake()
-      this.tweens.add({targets: coin, y: "-=200", duration: 500, ease: 'Linear'})
-      .on('complete',()=>{
-        coin.destroy()
-        })
-    }
-  }
-
 
 }
